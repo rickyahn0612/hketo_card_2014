edb7a166648a7a021e08f0bdb1d5281a58164670
rickyahn0612 <rickyahn0612@yahoo.com>
2014-12-15 15:57:14 -0800

    Site updated to 069b044

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,6 +1,11 @@
 (function() {
   $(function() {
-    return $("#video-container").fitVids();
+    $("#video-container").fitVids();
+    return $('#video-container img').on('click', function(e) {
+      e.preventDefault();
+      $(this).fadeOut(function() {});
+      return player.playVideo();
+    });
   });
 
 }).call(this);
